c4faa4c40947953effa6e979e8003e0c936bbaed
damian127721 <106865168+damian127721@users.noreply.github.com>
2023-07-14 11:10:10 +0200

    Score completed, frame prototype

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 const submitBtn = document.getElementById("submit-btn")
+const userScore = document.getElementById("score")
+const scoreFrame = document.getElementById("submitted-schedule-frame")
 const indexOfCorrectAnswers = [0, 2, 1, 1, 0, 2, 1]
+let userAnswers = []
 
 submitBtn.addEventListener("click", function(){
     console.clear()
     const questionOptions = document.querySelectorAll("input[id*='q1']").length
     const questionCount = document.getElementsByClassName("question").length
+    const lastOptionIndex = questionOptions - 1;
     let points = 0
+    userAnswers = []
     for (let i = 0; i < questionCount; ++i) {
         const questionGroup = []
         for (let j = 0; j < questionOptions; ++j) {
             questionGroup.push(document.getElementById(`q${i+1}a${j+1}`).checked)
             if (questionGroup[j] && j === indexOfCorrectAnswers[i]) {
+                userAnswers.push({index: j, decision: true})
                 points++
+            } else if (questionGroup[j]) {
+                userAnswers.push({index: j, decision: false})
+            } else if (j === lastOptionIndex && userAnswers[i] === undefined) {
+                userAnswers.push({index: "not chosen", decision: false})
             }
         }
         
     }
+    userScore.textContent = `${points}/${questionCount}`
     console.log(points)
+    console.log(userAnswers)
     
 })
 
